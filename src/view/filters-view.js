7bb9854c92ccb9cbf6937a29da2a3ef59a030474
4e4e4i <zazevang@gmail.com>
2020-12-01 13:07:01 +0300

    [module3-task1] added indents for templates

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -5,7 +5,14 @@ const createTripFiltersTemplate = (filterList, activeFilter = ``) => {
     `<form class="trip-filters" action="#" method="get">
       ${filterList.map((filter) => `
         <div class="trip-filters__filter">
-          <input id="filter-${filter}" ${filter === activeFilter ? `checked` : ``} class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}">
+          <input
+            id="filter-${filter}"
+            ${filter === activeFilter ? `checked` : ``}
+            class="trip-filters__filter-input  visually-hidden"
+            type="radio"
+            name="trip-filter"
+            value="${filter}"
+          >
           <label class="trip-filters__filter-label" for="filter-${filter}">${filter}</label>
         </div>
       `).join(``)}
